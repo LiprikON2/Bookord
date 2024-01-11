357f8676b4f8e009c0d0c5547a84a4f29fd2ac64
LiprikON2 <44316968+LiprikON2@users.noreply.github.com>
2024-01-11 18:12:22 +0300

    feat: save settings to localstorage

--- src/renderer/scenes/Settings/hooks/useSettingsStore.ts
+++ src/renderer/scenes/Settings/hooks/useSettingsStore.ts
@@ -1,67 +1,11 @@
-import { useEffect, useLayoutEffect } from "react";
-
-import { getSetting, setSetting, setInitStore, settingsStore } from "~/renderer/store";
+import { getSetting, getSettingsStore, setSetting } from "~/renderer/store";
 import { useMapSettings } from ".";
 import { SettingsMarkup } from "../Settings";
-import { toJS } from "mobx";
 
 export const useSettingsStore = (settingsMarkup: SettingsMarkup) => {
     const { mappedSettings } = useMapSettings(settingsMarkup);
-
-    const isLoading = !Object.values(settingsStore).length;
-
-    // TODO make it only for dev
-    useEffect(() => {
-        // @ts-ignore
-        window["store"] = toJS(settingsStore);
-        console.log("update");
-    }, [toJS(settingsStore)]);
-
-    useLayoutEffect(() => {
-        if (isLoading) {
-            const initSettings = Object.fromEntries(
-                Object.entries(mappedSettings).map(([tabHeading, settings]) => [
-                    tabHeading,
-                    Object.fromEntries(
-                        Object.entries(settings).map(([tab, settings]) => [
-                            tab,
-                            Object.fromEntries(
-                                Object.entries(settings).map(([section, settings]) => [
-                                    section,
-                                    Object.fromEntries(
-                                        settings.map((setting) => {
-                                            if (
-                                                !("defaultValue" in setting) &&
-                                                !("defaultChecked" in setting)
-                                            )
-                                                throw Error(
-                                                    "SettingsMarkup: either defaultValue or defaultChecked must be set"
-                                                );
-                                            return [
-                                                setting.label,
-                                                {
-                                                    disabled: false,
-                                                    ...("defaultValue" in setting && {
-                                                        // value: undefined,
-                                                        value: setting["defaultValue"],
-                                                    }),
-                                                    ...("defaultChecked" in setting && {
-                                                        // checked: undefined,
-                                                        checked: setting["defaultChecked"],
-                                                    }),
-                                                },
-                                            ];
-                                        })
-                                    ),
-                                ])
-                            ),
-                        ])
-                    ),
-                ])
-            );
-            setInitStore(initSettings);
-        }
-    }, []);
+    const store = getSettingsStore(mappedSettings);
+    const isLoading = !Object.keys(store.data ?? {}).length;
 
     return { setSetting, getSetting, isLoading };
 };
--- src/renderer/store/Settings.ts
+++ src/renderer/store/Settings.ts
@@ -1,19 +1,101 @@
-import { action, has, observable, set, toJS } from "mobx";
+import { action, observable, autorun, set, toJS } from "mobx";
+import _ from "lodash";
 
 import { type SettingsState } from "~/renderer/scenes/Settings";
+import { type MappedSettingsMarkup } from "../scenes/Settings/hooks";
+import { isDev } from "~/common/helpers";
 
-export const settingsStore = observable<SettingsState>({});
+declare global {
+    interface Window {
+        store: typeof settingsStore;
+    }
+}
 
-export const setInitStore = action((state: SettingsState) => set(settingsStore, "data", state));
-
-export const setSetting = action((settingKeyList: string[], key: string, value: any) => {
-    const nestedValue = settingKeyList.reduce((obj: any, k: any) => obj[k], settingsStore.data);
-    nestedValue[key] = value;
-    // console.log("set", key, "to", toJS(value));
-});
+let settingsStore = observable<SettingsState>({});
+let isInitialized = false;
+const settingsStoreKey = "settingsStore";
 
 export const getSetting = action((settingKeyList: string[]) => {
     const nestedValue = settingKeyList.reduce((obj: any, k: any) => obj[k], settingsStore.data);
     // console.log("get", settingKeyList[settingKeyList.length - 1], "is", toJS(nestedValue));
     return nestedValue;
 });
+
+export const setSetting = action((settingKeyList: string[], key: string, value: any) => {
+    const nestedValue = settingKeyList.reduce((obj: any, k: any) => obj[k], settingsStore.data);
+    // console.log("set", key, "to", toJS(value));
+    nestedValue[key] = value;
+});
+
+export const getSettingsStore = (mappedSettings: MappedSettingsMarkup) => {
+    if (!isInitialized) {
+        const defaultSettings = getDefaultSettings(mappedSettings);
+        const storedSettings = getStoredSettings()?.data ?? {};
+        const initSettings = _.merge({}, defaultSettings, storedSettings);
+
+        setSettingsStore(initSettings);
+        isInitialized = true;
+
+        autorun(() => {
+            setStoredSettings(settingsStore);
+
+            console.log(settingsStoreKey, "updated");
+            if (isDev()) window["store"] = toJS(settingsStore);
+        });
+    }
+
+    return settingsStore;
+};
+
+export const setSettingsStore = action((state: SettingsState) => set(settingsStore, "data", state));
+
+const getStoredSettings = () => {
+    return JSON.parse(localStorage.getItem(settingsStoreKey));
+};
+
+const setStoredSettings = (state: SettingsState) => {
+    localStorage.setItem(settingsStoreKey, JSON.stringify(toJS(state)));
+};
+
+const getDefaultSettings = (mappedSettings: MappedSettingsMarkup) => {
+    return Object.fromEntries(
+        Object.entries(mappedSettings).map(([tabHeading, settings]) => [
+            tabHeading,
+            Object.fromEntries(
+                Object.entries(settings).map(([tab, settings]) => [
+                    tab,
+                    Object.fromEntries(
+                        Object.entries(settings).map(([section, settings]) => [
+                            section,
+                            Object.fromEntries(
+                                settings.map((setting) => {
+                                    if (
+                                        !("defaultValue" in setting) &&
+                                        !("defaultChecked" in setting)
+                                    )
+                                        throw Error(
+                                            "SettingsMarkup: either defaultValue or defaultChecked must be set"
+                                        );
+                                    return [
+                                        setting.label,
+                                        {
+                                            disabled: false,
+                                            ...("defaultValue" in setting && {
+                                                // value: undefined,
+                                                value: setting["defaultValue"],
+                                            }),
+                                            ...("defaultChecked" in setting && {
+                                                // checked: undefined,
+                                                checked: setting["defaultChecked"],
+                                            }),
+                                        },
+                                    ];
+                                })
+                            ),
+                        ])
+                    ),
+                ])
+            ),
+        ])
+    );
+};
