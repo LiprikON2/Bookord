b7f2e368162583a863660d68cdfcd930b1671058
LiprikON2 <44316968+LiprikON2@users.noreply.github.com>
2024-04-26 21:16:11 +0300

    chore

---
+++ src/main/mainIpc.ts
@@ -0,0 +1,16 @@
+import { type BrowserWindow, ipcMain, shell } from "electron";
+
+export const registerMainIpc = (
+    mainWindow: BrowserWindow,
+    validateSender: (e: Electron.IpcMainInvokeEvent) => boolean
+) => {
+    console.log("");
+    // ipcMain.handle("test", async (e) => {
+    //     if (!validateSender(e)) return null;
+    //     // const res = fetch("https://google.com");
+    //     const res = new Promise((resolve, reject) => {
+    //         resolve("HUUUH!");
+    //     });
+    //     return res;
+    // });
+};
