c07b3347c48321bece6ad9adc25eec8c4b2ceeeb
Vadim <44316968+LiprikON2@users.noreply.github.com>
2025-02-23 16:59:08 +0300

    chore: fix postcss preprocessing inside storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,12 @@
 import type { StorybookConfig } from "@storybook/react-webpack5";
+import { RuleSetRule } from "webpack";
 
 import aliases from "../tools/webpack/webpack.aliases";
 import postcssConfig from "../tools/postcss/postcss.config";
+
+export const findRuleByTest = (rules: RuleSetRule[], test: RegExp): number => {
+    return rules.findIndex((rule) => rule?.test?.toString() === test.toString());
+};
 
 const config: StorybookConfig = {
     stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -13,29 +18,7 @@ const config: StorybookConfig = {
         "@storybook/addon-interactions",
 
         "storybook-dark-mode",
-        {
-            name: "@storybook/addon-styling-webpack",
-
-            options: {
-                rules: [
-                    {
-                        test: /\.css$/,
-                        sideEffects: true,
-                        use: [
-                            "style-loader",
-                            {
-                                loader: "css-loader",
-                                options: {},
-                            },
-                            // {
-                            //     loader: "postcss-loader",
-                            //     options: { postcssOptions: postcssConfig },
-                            // },
-                        ],
-                    },
-                ],
-            },
-        },
+        "@storybook/addon-styling-webpack",
     ],
     framework: {
         name: "@storybook/react-webpack5",
@@ -45,13 +28,38 @@ const config: StorybookConfig = {
         disableTelemetry: true,
     },
     webpackFinal: async (config) => {
+        // Import aliases from the project
         if (config.resolve) {
             config.resolve.alias = {
                 ...config.resolve.alias,
                 ...aliases,
             };
         }
-        console.log("config.module?.rules", config.module?.rules);
+
+        // Import postcss from the project
+        if (config?.module?.rules) {
+            const cssRuleIndex = findRuleByTest(config.module.rules as [], /\.css$/);
+
+            (config.module.rules[cssRuleIndex] as RuleSetRule).use = [
+                { loader: "style-loader" },
+                { loader: "css-loader" },
+                {
+                    loader: "postcss-loader",
+                    options: {
+                        postcssOptions: {
+                            config: false,
+                            plugins: postcssConfig.plugins,
+                        },
+                    },
+                },
+            ];
+        }
+
+        // console.log("Webpack rules:");
+        // config.module?.rules?.forEach((rule, i) => {
+        //     rule = rule as RuleSetRule;
+        //     console.log(`Rule #${i}:`, rule?.test?.toString(), rule?.use);
+        // });
 
         return config;
     },
